075e74ad86b9bc765a91b607f47893688524e96e
phuocding <phuocding@gmail.com>
2018-10-30 01:06:33 +0700

    Call the Web API with jQuery

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -19,7 +19,7 @@ namespace TodoApi.Controllers
             {
                 // Create a new TodoItem if collection is empty,
                 // which means you can't delete all TodoItems.
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Mr.Dao Xuan Hung" });
                 _context.SaveChanges();
             }
         }
